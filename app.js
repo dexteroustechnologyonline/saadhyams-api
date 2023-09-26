8749f2227d402c1e716cb894d89f471516e58653
Nalla Manoj Kumar <100987446+manojnalla098@users.noreply.github.com>
2023-09-26 15:52:28 +0530

    26-09-2

--- app.js
+++ app.js
@@ -24,19 +24,10 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
   // })
 }
 
-app.use(express.json());
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, parameterLimit: 500000, limit: '50mb' }))
 app.use(cookieParser());
-// app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({ limit: "10000kb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "10000kb", extended: true }));
-app.use(fileUpload({ useTempFiles: true }));
-
-
-const corsOptions = {
-  origin: "*",
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
 
 
 //Rotes Imports
